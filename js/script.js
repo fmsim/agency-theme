5f60ec7b5995f5373185f3713dd5d89e04bf056f
Frank Simon <f.m.sim@t-online.de>
2019-11-09 20:30:50 +0100

    added navigation

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 $(document).ready(function () {
-  /* navbar toggler */
-
+  /** NAVBAR TOGGLER **/
   $('.navbar-toggler').html("<i class='fa fa-arrow-down fa-3x' style='color:#f15025'></i>");
-
-  // smooth scroll
-
-
-
-
-
-
-
+  /** SMOTH SCROLL **/
 });
